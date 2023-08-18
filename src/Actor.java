003309270a135d130c3351c6fbe96c1473b3457c
Will <william.dixon@students.mq.edu.au>
2023-08-18 12:14:42 +1000

    completed week 4

--- src/Actor.java
+++ src/Actor.java
@@ -1,14 +1,17 @@
-import java.awt.Color;
+//import java.awt.Color;
 import java.awt.Graphics;
+import java.awt.Polygon;
+import java.util.ArrayList;
 
 public abstract class Actor {
-  Color color;
+  ArrayList<Polygon> polygons;
+  //Color color;
   Cell loc;
 
   public void paint(Graphics g) {
-    g.setColor(color);
-    g.fillRect(loc.x + 5, loc.y + 5, loc.width - 10, loc.height - 10);
-    g.setColor(Color.GRAY);
-    g.drawRect(loc.x + 5, loc.y + 5, loc.width - 10, loc.height - 10);
+    for (int i = 0; i < polygons.size(); i++) {
+      //g.setColor(color);
+      g.drawPolygon(polygons.get(i));
+    }
   }
 }
--- src/Bird.java
+++ src/Bird.java
@@ -1,8 +1,27 @@
-import java.awt.Color;
+//import java.awt.Color;
+import java.util.ArrayList;
+import java.awt.Polygon;
 
 public class Bird extends Actor {
   public Bird(Cell inLoc) {
     loc = inLoc;
-    color = Color.GREEN;
+    polygons = new ArrayList<>();
+    //color = Color.GREEN;
+    Polygon wing1 = new Polygon();
+    wing1.addPoint(loc.x + 5, loc.y + 5);
+    wing1.addPoint(loc.x + 15, loc.y + 17);
+    wing1.addPoint(loc.x + 5, loc.y + 17);
+    Polygon wing2 = new Polygon();
+    wing2.addPoint(loc.x + 30, loc.y + 5);
+    wing2.addPoint(loc.x + 20, loc.y + 17);
+    wing2.addPoint(loc.x + 30, loc.y + 17);
+    Polygon body = new Polygon();
+    body.addPoint(loc.x + 15, loc.y + 10);
+    body.addPoint(loc.x + 20, loc.y + 10);
+    body.addPoint(loc.x + 20, loc.y + 25);
+    body.addPoint(loc.x + 15, loc.y + 25);
+    polygons.add(wing1);
+    polygons.add(wing2);
+    polygons.add(body);
   }
 }
